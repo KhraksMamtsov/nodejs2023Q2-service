2754109d069f02f66eb89ad44017a1f58c0067e3
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2023-07-27 14:07:26 +0200

    feat: annotate user resource

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: "The user's login",
+  })
   @IsString()
   login: string;
 
+  @ApiProperty({
+    description: "The user's password",
+  })
   @IsString()
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,45 +15,97 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { StatusCodes } from 'http-status-codes/build/cjs/status-codes';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
   @ApiOperation({
     description: 'Get all users',
     summary: 'Get all users',
   })
+  @ApiOkResponse({
+    description: 'Successful operation',
+    type: [User],
+  })
+  @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  @Post()
-  @HttpCode(StatusCodes.CREATED)
   @ApiOperation({
     description: 'Create a new user',
     summary: 'Create user',
   })
+  @ApiCreatedResponse({
+    description: 'The user has been created',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'Body does not contain required fields',
+  })
+  @Post()
+  @HttpCode(StatusCodes.CREATED)
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get(':id')
-  async findOne(@Param('id', ParseUUIDPipe) id: string) {
-    const maybeUser = await this.userService.findOne(id);
+  @ApiOperation({
+    summary: 'Get single user by id',
+    description: 'Get single user by id',
+  })
+  @ApiOkResponse({
+    description: 'Successful operation',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'userId is invalid (not uuid)',
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
+  @Get(':userId')
+  async findOne(@Param('userId', ParseUUIDPipe) userId: string) {
+    const maybeUser = await this.userService.findOne(userId);
     if (maybeUser === null) {
-      throw new NotFoundException(`There is no user with id ${id}`);
+      throw new NotFoundException(`There is no user with id ${userId}`);
     } else {
       return maybeUser;
     }
   }
 
-  @Put(':id')
+  @ApiOperation({
+    summary: "Update a user's password",
+    description: "Updates a user's password by ID",
+  })
+  @ApiOkResponse({
+    description: 'The user has been updated',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: 'userId is invalid (not uuid)',
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
+  @ApiForbiddenResponse({
+    description: 'Old password is wrong',
+  })
+  @Put(':userId')
   async update(
-    @Param('id', ParseUUIDPipe) id: string,
+    @Param('userId', ParseUUIDPipe) id: string,
     @Body() updateUserDto: UpdatePasswordDto,
   ) {
     const maybeUpdateResult = await this.userService.update(id, updateUserDto);
@@ -69,9 +121,20 @@ export class UserController {
     }
   }
 
-  @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete user',
+    description: 'Deletes user by ID',
+  })
+  @ApiNoContentResponse({
+    description: 'The user has been deleted',
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
+  @Delete(':userId')
   @HttpCode(StatusCodes.NO_CONTENT)
-  async remove(@Param('id', ParseUUIDPipe) id: string) {
+  async remove(@Param('userId', ParseUUIDPipe) id: string) {
     const maybeUser = await this.userService.remove(id);
 
     if (maybeUser === null) {
